d998ff0c2e4d71826a5781820e6e72947dd4657d
Steven <steven.chambers@cox.net>
2015-09-12 11:37:57 -0500

    add support library v4 to build and start on notifications
    suppoer library v4 is required for using api agnostic notifications (the notificationCompat class).
    CallForStudentQuestionResponseReceiver will be responsible for catching an intent that tells the
    student that the teacher has asked for a response to the question.

---
+++ app/src/main/java/edu/uco/schambers/classmate/BroadcastReceivers/CallForStudentQuestionResponseReceiver.java
@@ -0,0 +1,20 @@
+package edu.uco.schambers.classmate.BroadcastReceivers;
+
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+public class CallForStudentQuestionResponseReceiver extends BroadcastReceiver
+{
+    public CallForStudentQuestionResponseReceiver()
+    {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
