b655a68dc20547b74412ef0445f3a8661fee60cb
Rayan Al-Hammami <ralhamami@gmail.com>
2015-09-14 23:39:43 -0500

    Spinner class selection box added

--- app/src/main/java/edu/uco/schambers/classmate/Fragments/Debug.java
+++ app/src/main/java/edu/uco/schambers/classmate/Fragments/Debug.java
@@ -152,9 +152,8 @@ public class Debug extends Fragment
             @Override
             public void onClick(View v)
             {
-                showRequestToast();
-                //create an instance of your fragment here and pass it to the next function
-                launchFragment(null);
+                Fragment teacherAttendance = TeacherAttendance.newInstance("test","test");
+                launchFragment(teacherAttendance);
             }
         });
     }
