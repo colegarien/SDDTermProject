c6e536b71477c95cec4ad9cbadf9ecd2dad40e3f
nelalison <calitova@gmail.com>
2015-09-22 23:18:47 -0500

    cleaned up code in DataRepo, User and Login

--- app/src/main/java/edu/uco/schambers/classmate/Database/User.java
+++ app/src/main/java/edu/uco/schambers/classmate/Database/User.java
@@ -7,14 +7,10 @@ import java.util.regex.Pattern;
 
 public class User implements Serializable {
 	private int id;
-	private String username;
+	private String name;
 	private String password;
-	private String fname;
-	private String lname;
 	private boolean Student;
-	private String phone;
 	private String email;
-	private boolean Male;
 
 	public int getId() {
 		return id;
@@ -24,12 +20,12 @@ public class User implements Serializable {
 		this.id = id;
 	}
 
-	public String getUsername() {
-		return username;
+	public String getName() {
+		return name;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getPassword() {
@@ -37,23 +33,7 @@ public class User implements Serializable {
 	}
 
 	public void setPassword(String password) {
-		this.password = SHA256Encrypt.encrypt(password);
-	}
-
-	public String getFname() {
-		return fname;
-	}
-
-	public void setFname(String fname) {
-		this.fname = fname;
-	}
-
-	public String getLname() {
-		return lname;
-	}
-
-	public void setLname(String lname) {
-		this.lname = lname;
+		this.password = password;
 	}
 
 	public boolean isStudent() {
@@ -64,28 +44,12 @@ public class User implements Serializable {
 		this.Student = Student;
 	}
 
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
 	public String getEmail() {
 		return email;
 	}
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public boolean isMale() {
-		return Male;
-	}
-
-	public void setIsMale(boolean Male) {
-		this.Male = Male;
 	}
 
 	private static final String NAME = "[a-zA-Z ]+";
