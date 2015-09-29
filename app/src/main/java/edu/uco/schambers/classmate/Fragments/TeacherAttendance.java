f9366abfb4f31a5c892019b099b7a229d2e029e7
Rayan Al-Hammami <ralhamami@gmail.com>
2015-09-29 05:55:01 -0500

    Split Last Name,First Name and Additional Improvements
    -Added Last name to Student test class, as well as all necessary modifications to support it
    -Added Last name to group of dynamically allocated rows as well as a new column header
    -Removed "All" as an option for course selection based on Steven's insightful recommendation
    -Cleaned up some unused code and imports in various positions
    -Removed Labels for Spinners, and instead added "Labels" to the Spinner items themselves
    -Modified sortByName to sort by last name instead of first name
    -Made some various adjustments to the layout properties

--- app/src/main/java/edu/uco/schambers/classmate/Fragments/TeacherAttendance.java
+++ app/src/main/java/edu/uco/schambers/classmate/Fragments/TeacherAttendance.java
@@ -7,31 +7,20 @@
  */
 package edu.uco.schambers.classmate.Fragments;
 
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsoluteLayout;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import edu.uco.schambers.classmate.R;
@@ -47,14 +36,17 @@ import edu.uco.schambers.classmate.R;
 //TESTING CLASS - Student objects are created and fill an array that populates
 //the main table
 class Student{
-    String studentName;
+    String studentFName;
+    String studentLName;
     int numberAbsences;
     String course;
-    public String getStudentName(){return studentName;}
+    public String getStudentFName(){return studentFName;}
+    public String getStudentLName(){return studentLName;}
     public int getNumberAbsences(){return numberAbsences;}
     public String getCourse(){return course;}
-    Student(String studentName, int numberAbsences,String course){
-        this.studentName = studentName;
+    Student(String studentFName, String studentLName, int numberAbsences,String course){
+        this.studentFName = studentFName;
+        this.studentLName = studentLName;
         this.numberAbsences=numberAbsences;
         this.course = course;
     }
@@ -109,36 +101,36 @@ public class TeacherAttendance extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        students[0] = new Student("Rayan Al-Hammami",5, "Data Structures");
-        students[1] = new Student("Rajiv Mancharamo",3, "Data Structures");
-        students[2] = new Student("Mossi Alobaid",4, "Data Structures");
-        students[3] = new Student("Sana Mumallah",7,"Programming I");
-        students[4] = new Student("Zakria Hamdi",2,"Programming I");
-        students[5] = new Student("Josh Johnson",5,"Programming I");
-        students[6] = new Student("Eric Miller",3, "Programming I");
-        students[7] = new Student("Tim Thompson",4,"Programming I");
-        students[8] = new Student("Tom Timson",7,"Programming II");
-        students[9] = new Student("Ralph Matheson",2,"Programming II");
-        students[10] = new Student("Pete Peterson",5,"Programming II");
-        students[11] = new Student("Frank Wilson",3,"Programming II");
-        students[12] = new Student("James Taylor",4,"Computer Organization");
-        students[13] = new Student("Jimmy Page",7,"Computer Organization");
-        students[14] = new Student("Robert Plant",2,"Computer Organization");
-        students[15] = new Student("John Bonham",5,"Computer Organization");
-        students[16] = new Student("John-Paul Jones",3,"Computer Organization");
-        students[17] = new Student("Ozzy Smith",4,"Data Structures");
-        students[18] = new Student("Randy Teller",7,"Data Structures");
-        students[19] = new Student("Sam Smith",2,"Data Structures");
-        students[20] = new Student("Dean Howard",5,"Data Structures");
-        students[21] = new Student("Paul Mithcell",3,"Data Structures");
-        students[22] = new Student("Will Black",4,"Programming I");
-        students[23] = new Student("Isaac Turner",7,"Programming I");
-        students[24] = new Student("Abdualaziz Ahmed",2,"Programming I");
-        students[25] = new Student("Zachary Lee",5,"Programming I");
-        students[26] = new Student("Tom Hanks",3,"Programming II");
-        students[27] = new Student("Albert Albertson",4,"Programming II");
-        students[28] = new Student("Raymond Butler",7,"Programming II");
-        students[29] = new Student("Howard Howardson",2,"Programming II");
+        students[0] = new Student("Rayan","Al-Hammami",5, "Data Structures");
+        students[1] = new Student("Rajiv","Mancharamo",3, "Data Structures");
+        students[2] = new Student("Mossi","Alobaid",4, "Data Structures");
+        students[3] = new Student("Sana","Mumallah",7,"Programming I");
+        students[4] = new Student("Zakria","Hamdi",2,"Programming I");
+        students[5] = new Student("Josh","Johnson",5,"Programming I");
+        students[6] = new Student("Eric","Miller",3, "Programming I");
+        students[7] = new Student("Tim","Thompson",4,"Programming I");
+        students[8] = new Student("Tom","Timson",7,"Programming II");
+        students[9] = new Student("Ralph","Matheson",2,"Programming II");
+        students[10] = new Student("Pete","Peterson",5,"Programming II");
+        students[11] = new Student("Frank","Wilson",3,"Programming II");
+        students[12] = new Student("James","Taylor",4,"Computer Organization");
+        students[13] = new Student("Jimmy","Page",7,"Computer Organization");
+        students[14] = new Student("Robert","Plant",2,"Computer Organization");
+        students[15] = new Student("John","Bonham",5,"Computer Organization");
+        students[16] = new Student("John-Paul","Jones",3,"Computer Organization");
+        students[17] = new Student("Ozzy","Smith",4,"Data Structures");
+        students[18] = new Student("Randy","Teller",7,"Data Structures");
+        students[19] = new Student("Sam","Smith",2,"Data Structures");
+        students[20] = new Student("Dean","Howard",5,"Data Structures");
+        students[21] = new Student("Paul","Mithcell",3,"Data Structures");
+        students[22] = new Student("Will","Black",4,"Programming I");
+        students[23] = new Student("Isaac","Turner",7,"Programming I");
+        students[24] = new Student("Abdualaziz","Ahmed",2,"Programming I");
+        students[25] = new Student("Zachary","Lee",5,"Programming I");
+        students[26] = new Student("Tom","Hanks",3,"Programming II");
+        students[27] = new Student("Albert","Albertson",4,"Programming II");
+        students[28] = new Student("Raymond","Butler",7,"Programming II");
+        students[29] = new Student("Howard","Howardson",2,"Programming II");
     }
 
 
@@ -157,7 +149,8 @@ public class TeacherAttendance extends Fragment {
     {
         final List<String> spinnerArray =  new ArrayList<String>();
         //Hard-coded temporarily for testing purposes
-        spinnerArray.add("All");
+        spinnerArray.add("Select Course..");
+        //spinnerArray.add("All");
         spinnerArray.add("Data Structures");
         spinnerArray.add("Programming I");
         spinnerArray.add("Programming II");
@@ -172,7 +165,8 @@ public class TeacherAttendance extends Fragment {
 
         //For sorting spinner
         final List<String> spinnerArraySort =  new ArrayList<String>();
-        //Hard-coded temporatily for testing purposes
+        //Hard-coded temporarily for testing purposes
+        spinnerArraySort.add("Sort..");
         spinnerArraySort.add("By Name A-Z");
         spinnerArraySort.add("By Name Z-A");
         spinnerArraySort.add("By Absences Low-High");
@@ -208,7 +202,7 @@ public class TeacherAttendance extends Fragment {
                 else if (parent.getSelectedItem().equals("By Absences Low-High")){
                     sortByAbsences("asc");
                 }
-                else{
+                else if (parent.getSelectedItem().equals("By Absences High-Low")){
                     sortByAbsences("des");
                 }
                 buildTable(rootView,spinnerArray,sItems.getSelectedItemPosition());
@@ -228,13 +222,16 @@ public class TeacherAttendance extends Fragment {
         teacherAttendanceTable.bringToFront();
         teacherAttendanceTable.removeAllViews();
         for (int i = 0; i < students.length; i++) {
-            if (students[i].getCourse().equals(spinnerArray.get(position)) ||
-                    spinnerArray.get(position).equals("All")) {
+            if (students[i].getCourse().equals(spinnerArray.get(position)) /*||
+                    spinnerArray.get(position).equals("All")*/) {
                 TableRow tr = new TableRow(view.getContext());
+                TextView c0 = new TextView(view.getContext());
+                c0.setText(students[i].getStudentLName());
                 TextView c1 = new TextView(view.getContext());
-                c1.setText(students[i].getStudentName());
+                c1.setText(students[i].getStudentFName());
                 TextView c2 = new TextView(view.getContext());
                 c2.setText(String.valueOf(students[i].getNumberAbsences()));
+                tr.addView(c0);
                 tr.addView(c1);
                 tr.addView(c2);
                 teacherAttendanceTable.addView(tr);
@@ -248,7 +245,7 @@ public class TeacherAttendance extends Fragment {
         for (int m = n; m >= 0; m--) {
             for (int i = 0; i < n - 1; i++) {
                 k = i + 1;
-                int comp = students[i].getStudentName().compareTo(students[k].getStudentName());
+                int comp = students[i].getStudentLName().compareTo(students[k].getStudentLName());
                 if (sortType.equals("asc")) {
                     if (comp > 0) {
                         swapNumbers(i, k, students);
