08e0d3c0e90ec157e4fb6ce37fd63e70599ecb18
Matt McHughes <liveyourheart@gmail.com>
2015-09-28 19:07:58 -0500

    user info bug fixed

--- app/src/main/java/edu/uco/schambers/classmate/Activites/MainActivity.java
+++ app/src/main/java/edu/uco/schambers/classmate/Activites/MainActivity.java
@@ -121,8 +121,12 @@ public class MainActivity extends Activity implements StudentResponseFragment.On
         {
             sp = this.getSharedPreferences(MyPREFS, Context.MODE_PRIVATE);
             editor = sp.edit();
+            Log.d("after", sp.getString("USER_KEY", ""));
+            editor.remove("USER_KEY");
             editor.clear();
+            editor.putString("USER_KEY", "");
             editor.commit();
+            Log.d("after", sp.getString("USER_KEY", ""));
             FragmentTransaction trans = getFragmentManager().beginTransaction();
             Fragment AuthFragment = new Auth();
             trans.replace(R.id.fragment_container, AuthFragment).addToBackStack(null);
