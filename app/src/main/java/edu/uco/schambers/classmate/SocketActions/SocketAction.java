b0c730f9360073b8cc89ccc4e73c381c9ec514ae
Steven <steven.chambers@cox.net>
2015-10-03 14:25:47 -0500

    implement sending questions with socket action

--- app/src/main/java/edu/uco/schambers/classmate/SocketActions/SocketAction.java
+++ app/src/main/java/edu/uco/schambers/classmate/SocketActions/SocketAction.java
@@ -1,17 +1,45 @@
 package edu.uco.schambers.classmate.SocketActions;
 
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Created by Steven Chambers on 10/3/2015.
  */
 public abstract class SocketAction
 {
-   abstract void setUpSocket();
-   abstract void performAction();
-   abstract void tearDownSocket();
+   static final int QUESTIONS_PORT_NUMBER = 8080;
+
+   abstract void setUpSocket() throws  IOException;
+   abstract void performAction() throws IOException;
+   abstract void tearDownSocket() throws  IOException;
    void execute()
    {
-      setUpSocket();
-      performAction();
-      tearDownSocket();
+      Thread executionThread = new Thread(new Runnable()
+      {
+         @Override
+         public void run()
+         {
+            try
+            {
+               setUpSocket();
+               performAction();
+               tearDownSocket();
+            }
+            catch (UnknownHostException e)
+            {
+               Log.d("StudentSendAction", "Host not found. Exception: " + e.toString());
+            }
+            catch(IOException e)
+            {
+               Log.d("StudentSendAction", "IOException: " + e.toString());
+            }
+         }
+      });
+      executionThread.start();
    }
 }
--- app/src/main/java/edu/uco/schambers/classmate/SocketActions/StudentSendQuestionAction.java
+++ app/src/main/java/edu/uco/schambers/classmate/SocketActions/StudentSendQuestionAction.java
@@ -1,25 +1,73 @@
 package edu.uco.schambers.classmate.SocketActions;
 
+import android.util.Log;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import edu.uco.schambers.classmate.Models.Questions.IQuestion;
+
 /**
  * Created by Steven Chambers on 10/3/2015.
  */
 public class StudentSendQuestionAction extends SocketAction
 {
-    @Override
-    void setUpSocket()
-    {
+    Socket socket;
+    ObjectOutputStream objectOutputStream;
+    DataInputStream dataInputStream;
 
+    IQuestion questionToSend;
+
+    boolean questionSentSuccessfully;
+
+    @Override
+    void setUpSocket() throws UnknownHostException, IOException
+    {
+        socket = new Socket("localhost", QUESTIONS_PORT_NUMBER);
+        objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        dataInputStream = new DataInputStream(socket.getInputStream());
     }
 
     @Override
-    void performAction()
+    void performAction() throws IOException
     {
-
+        if (questionToSend != null)
+        {
+            objectOutputStream.writeObject(questionToSend);
+        }
+        questionSentSuccessfully = dataInputStream.readBoolean();
     }
 
     @Override
-    void tearDownSocket()
+    void tearDownSocket() throws IOException
     {
+        if(socket != null)
+        {
+            socket.close();
+        }
+        if(dataInputStream != null)
+        {
+            dataInputStream.close();
+        }
+        if(objectOutputStream != null)
+        {
+            objectOutputStream.close();
+        }
 
+    }
+
+    public void setQuestionToSend(IQuestion questionToSend)
+    {
+        this.questionToSend = questionToSend;
+    }
+
+    public boolean isQuestionSentSuccessfully()
+    {
+        return questionSentSuccessfully;
     }
 }
