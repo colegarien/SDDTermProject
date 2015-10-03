36ee2df08fe0d6dcad7eb97afb0d73d921ec684f
Steven <steven.chambers@cox.net>
2015-10-03 14:38:10 -0500

    implement on question received listener
    this is in preparation of implementing a socket action class that will listen for incoming questions. We will be using the observer (or listener) pattern here in order for the base service to be notified when a new question is received.

---
+++ app/src/main/java/edu/uco/schambers/classmate/ListenerInterfaces/OnQuestionReceivedListener.java
@@ -0,0 +1,11 @@
+package edu.uco.schambers.classmate.ListenerInterfaces;
+
+import edu.uco.schambers.classmate.Models.Questions.IQuestion;
+
+/**
+ * Created by Steven Chambers on 10/3/2015.
+ */
+public interface OnQuestionReceivedListener
+{
+    void onQuestionReceived(IQuestion q);
+}
--- app/src/main/java/edu/uco/schambers/classmate/Services/StudentQuestionService.java
+++ app/src/main/java/edu/uco/schambers/classmate/Services/StudentQuestionService.java
@@ -13,11 +13,11 @@ import android.widget.Toast;
 
 import edu.uco.schambers.classmate.Activites.MainActivity;
 import edu.uco.schambers.classmate.Fragments.StudentResponseFragment;
-import edu.uco.schambers.classmate.Models.Questions.DefaultMultiChoiceQuestion;
+import edu.uco.schambers.classmate.ListenerInterfaces.OnQuestionReceivedListener;
 import edu.uco.schambers.classmate.Models.Questions.IQuestion;
 import edu.uco.schambers.classmate.R;
 
-public class StudentQuestionService extends Service
+public class StudentQuestionService extends Service implements OnQuestionReceivedListener
 {
     public static final String ACTION_NOTIFY_QUESTION_RECEIVED = "edu.uco.schambers.classmate.Services.StudentQuestionService.ACTION_NOTIFY_QUESTION_RECEIVED";
     public static final String ACTION_REQUEST_QUESTION_RESPONSE = "edu.uco.schambers.classmate.Services.StudentQuestionService.ACTION_REQUEST_QUESTION_RESPONSE";
@@ -108,5 +108,12 @@ public class StudentQuestionService extends Service
     {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onQuestionReceived(IQuestion question)
+    {
+        Intent intent = getNewSendQuestionIntent(this, question);
+        startService(intent);
     }
 }
