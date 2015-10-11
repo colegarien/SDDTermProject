3eb8db71bc6bdc513097a67932c4ff52a6f0eab0
Rayan Al-Hammami <ralhamami@gmail.com>
2015-10-11 14:06:18 -0500

    Cleaned Up Teacher Attendance Table and Added Semester/Year Spinners
    -Modified table alignment and colors
    -Added Spinners for Semester/Year
    -Added cell_shape.xml for creating cell shaped table header for now

--- app/src/main/java/edu/uco/schambers/classmate/Fragments/TeacherAttendance.java
+++ app/src/main/java/edu/uco/schambers/classmate/Fragments/TeacherAttendance.java
@@ -11,6 +11,8 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
