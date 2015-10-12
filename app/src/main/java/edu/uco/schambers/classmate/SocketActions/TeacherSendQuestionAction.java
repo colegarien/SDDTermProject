1d9558ca999d05de323b1ccf0b62477d30f1a4fc
Connor Archer <carcher2@uco.edu>
2015-10-11 22:37:49 -0500

    Add teacher service and corresponding socket actions
    This commit adds the TeacherQuestionService to handle communication with Steven's StudentQuestionService and facilitate communication and transportation of the IQuestion object to the Student service and then recieve the answer back from the student's device. It implements the corresponding sockets mirroring Steven's functinality: TeacherSendQuestionAction and TeacherAcceptQuestionAction. These and the service implement the predefined interfaces established by both steven and cole as well as following the Template for a SocketAction.

---
+++ app/src/main/java/edu/uco/schambers/classmate/SocketActions/TeacherSendQuestionAction.java
@@ -0,0 +1,71 @@
+package edu.uco.schambers.classmate.SocketActions;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import edu.uco.schambers.classmate.ListenerInterfaces.OnQuestionReceivedListener;
+import edu.uco.schambers.classmate.Models.Questions.IQuestion;
+
+/**
+ * Created by Steven Chambers on 10/3/2015.
+ */
+public class TeacherSendQuestionAction extends SocketAction
+{
+    ObjectOutputStream objectOutputStream;
+    DataInputStream dataInputStream;
+    private String domain = "localhost";
+
+    IQuestion questionToSend;
+    OnQuestionReceivedListener questionReceivedListener;
+
+    boolean questionSentSuccessfully;
+
+    public TeacherSendQuestionAction(IQuestion question, OnQuestionReceivedListener listener)
+    {
+        this.questionToSend = question;
+        this.questionReceivedListener = listener;
+    }
+
+    @Override
+    void setUpSocket() throws UnknownHostException, IOException
+    {
+        socket = new Socket(domain, QUESTIONS_PORT_NUMBER);
+        objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        dataInputStream = new DataInputStream(socket.getInputStream());
+    }
+
+    @Override
+    void performAction() throws IOException
+    {
+        if (questionToSend != null)
+        {
+            objectOutputStream.writeObject(questionToSend);
+        }
+        questionSentSuccessfully = dataInputStream.readBoolean();
+        if(questionSentSuccessfully)
+        {
+            questionReceivedListener.onQuestionSentSuccessfully(domain,QUESTIONS_PORT_NUMBER);
+        }
+    }
+
+    @Override
+    void tearDownSocket() throws IOException
+    {
+        if(socket != null)
+        {
+            socket.close();
+        }
+        if(dataInputStream != null)
+        {
+            dataInputStream.close();
+        }
+        if(objectOutputStream != null)
+        {
+            objectOutputStream.close();
+        }
+
+    }
+}
