d5e6e25b6396028dff0a4fa7c5cb0e6d5e61406b
Matt McHughes <liveyourheart@gmail.com>
2015-10-11 21:30:43 -0500

    updated to work with api 18

--- app/src/main/java/edu/uco/schambers/classmate/Activites/MainActivity.java
+++ app/src/main/java/edu/uco/schambers/classmate/Activites/MainActivity.java
@@ -138,8 +138,6 @@ public class MainActivity extends Activity implements StudentResponseFragment.On
             sp = this.getSharedPreferences(MyPREFS, Context.MODE_PRIVATE);
             editor = sp.edit();
             editor.remove("AUTH_TOKEN");
-            editor.clear();
-            editor.putString("AUTH_TOKEN", null);
             editor.commit();
 
             FragmentTransaction trans = getFragmentManager().beginTransaction();
