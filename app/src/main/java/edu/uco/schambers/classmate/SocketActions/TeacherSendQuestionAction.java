339ffe83fec7c1f986d9dca33ebc450fb4ae9082
Connor Archer <carcher2@uco.edu>
2015-10-13 10:38:01 -0500

    Add multi-device functionality for sending questions
    This commit adds multi-device functionality to the TeacherQuestion module; specifically the TeacherSendQuestionAction. This works by pulling a list of IP addresses from the IPAddressManager and building a socket list with corresponding input and output streams for all students.  Currently it only supports SENDING questions to multiple student devices, I don't think this will automatically work for accepting answers; I just want Steven to take a look at this and make sure this will work before i go to the trouble of incorrectly setting up the receiving end. To clarify, it accepts the immediate return from multiple devices (such as the boolean check for reception) but it is not set up to process and return IQuestion data, as that is handled by the TeacherRecieveQuestionAction which still only supports 1 device -- which might work for testing purposes honestly.

--- app/src/main/java/edu/uco/schambers/classmate/SocketActions/TeacherSendQuestionAction.java
+++ app/src/main/java/edu/uco/schambers/classmate/SocketActions/TeacherSendQuestionAction.java
@@ -3,23 +3,32 @@ package edu.uco.schambers.classmate.SocketActions;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import edu.uco.schambers.classmate.ListenerInterfaces.OnQuestionReceivedListener;
 import edu.uco.schambers.classmate.Models.Questions.IQuestion;
+import edu.uco.schambers.classmate.ObservableManagers.IPAddressManager;
 
 /**
  * Created by Steven Chambers on 10/3/2015.
  */
 public class TeacherSendQuestionAction extends SocketAction
 {
-    ObjectOutputStream objectOutputStream;
-    DataInputStream dataInputStream;
-    private String domain = "localhost";
+    //ObjectOutputStream objectOutputStream;
+    //DataInputStream dataInputStream;
+    //private String domain = "localhost";
 
     IQuestion questionToSend;
     OnQuestionReceivedListener questionReceivedListener;
+
+
+    ArrayList<Socket> classSocketList;//ArrayList<Socket> of student ips
+    ArrayList<ObjectOutputStream> objectOutputStreams;//ArrayList<ObjectOutputStream> student output streams
+    ArrayList<DataInputStream> dataInputStreams;//ArrayList<ObjectOutputStream> student output streams
 
     boolean questionSentSuccessfully;
 
@@ -32,39 +41,117 @@ public class TeacherSendQuestionAction extends SocketAction
     @Override
     void setUpSocket() throws UnknownHostException, IOException
     {
-        socket = new Socket(domain, QUESTIONS_PORT_NUMBER);
-        objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-        dataInputStream = new DataInputStream(socket.getInputStream());
+        //socket = new Socket(domain, QUESTIONS_PORT_NUMBER);
+
+        //Setup classSocketList and corresponding dataInput/objectOut streams for
+        // students listed in IpAddressManager.
+        generateLists();
     }
+
+    private void generateLists() throws IOException {
+        //TODO: refactor, consider data storage location, consider data type
+        //Will Clean this up later
+        classSocketList = new ArrayList<>();
+        dataInputStreams = new ArrayList<>();
+        objectOutputStreams = new ArrayList<>();
+
+        //ArrayList<String> classIPList = new ArrayList<>();
+        ArrayList<InetAddress> classIPList = IPAddressManager.getInstance().getStudentAddresses();
+        //Iterator
+
+        Socket ts;
+        Iterator<InetAddress> ipIterator = classIPList.iterator();
+        while (ipIterator.hasNext()){
+            InetAddress ip = ipIterator.next();
+            ts = new Socket(ip, QUESTIONS_PORT_NUMBER);
+            classSocketList.add(ts);
+            objectOutputStreams.add(new ObjectOutputStream(ts.getOutputStream()));
+            dataInputStreams.add(new DataInputStream(ts.getInputStream()));
+        }
+    }
+
+    /*private void verifySocketStreams(){
+        Iterator<Socket> classSocketIterator = classSocketList.iterator();
+
+        Socket ts;
+        while(classSocketIterator.hasNext()){
+            ts = classSocketIterator.next();
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            dataInputStream = new DataInputStream(socket.getInputStream());
+
+        }
+    }*/
 
     @Override
     void performAction() throws IOException
     {
         if (questionToSend != null)
         {
-            objectOutputStream.writeObject(questionToSend);
+            //objectOutputStream.writeObject(questionToSend);
+            sendQuestions();
         }
-        questionSentSuccessfully = dataInputStream.readBoolean();
+
+        //questionsSentSuccessfully = checkAllQuestionsSent();
+        //questionSentSuccessfully = dataInputStream.readBoolean();
+
         if(questionSentSuccessfully)
         {
-            questionReceivedListener.onQuestionSentSuccessfully(domain,QUESTIONS_PORT_NUMBER);
+            questionReceivedListener.onQuestionSentSuccessfully("student x, " +
+                    "could be modified to display if neccesary",QUESTIONS_PORT_NUMBER);
+        }
+    }
+
+    private void sendQuestions() throws IOException {
+        //TODO: modify lists to be iterable and/or encapsulate data another way
+        //for each loop, dynamically allocate/create I/O streams
+
+        Socket ts;
+        ObjectOutputStream oos;
+        DataInputStream dis;
+        for (int i = 0; i<classSocketList.size(); i++){
+            ts = classSocketList.get(i); //don't need, life cycle of socket streams tied to sockets?
+            oos = objectOutputStreams.get(i);
+            dis = dataInputStreams.get(i);
+            oos.writeObject(questionToSend);
+            questionSentSuccessfully = dis.readBoolean();
         }
     }
 
     @Override
     void tearDownSocket() throws IOException
     {
-        if(socket != null)
+        //Refactor methods to reduce repeated code
+        if(classSocketList != null)
         {
+            for (Socket s :
+                    classSocketList) {
+                if (s != null){
+                    s.close();
+                }
+
+            }
             socket.close();
         }
-        if(dataInputStream != null)
+        if(dataInputStreams != null)
         {
-            dataInputStream.close();
+            for (DataInputStream dis :
+                    dataInputStreams) {
+                if (dis != null){
+                    dis.close();
+                }
+
+            }
         }
-        if(objectOutputStream != null)
+        if(objectOutputStreams != null)
         {
-            objectOutputStream.close();
+            for (ObjectOutputStream oos :
+                    objectOutputStreams) {
+                if (oos != null){
+                    oos.close();
+                }
+
+            }
+
         }
 
     }
